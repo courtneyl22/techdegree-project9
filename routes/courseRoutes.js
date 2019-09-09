7ae6714d94474732a98685f5b3da0b64547c4250
courtneyl22 <courtneyluc11@gmail.com>
2019-09-09 14:25:04 -0400

    implemented authentication

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -6,24 +6,46 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models').User;
 const Course = require('../models').Course;
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
 
-// //body parsing
-// const bodyParser = require('body-parser');
-// router.use(bodyParser.json());
-// router.use(bodyParser.urlencoded({ extended: false }));
+/**
+ * authenticating the user 
+*/
 
-const asyncHandler = cb => {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch(err) {
-      console.log('There was an error with the application');
-      next(err);
+const authenticatedUser = async (req, res, next) => {
+   let message = null;
+  const credentials = auth(req);
+  if (credentials) {
+    const user = await User.findOne({ where: {emailAddress: credentials.name} });
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
     }
+  } else {
+    message = 'Auth header not found';
   }
-}
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
 
-router.get('/courses', asyncHandler( async (req, res) => {
+/*
+ * ROUTES *
+*/
+
+//returns a list of courses (including the user that owns each course)
+router.get('/courses', async (req, res) => {
   const course = await Course.findAll({
     include:[
     {
@@ -33,8 +55,9 @@ router.get('/courses', asyncHandler( async (req, res) => {
     ]
   })
   res.json(course);
-}));
+});
 
+//returns a course (including the user that owns the course) for the provided course ID
 router.get('/courses/:id', async (req, res) => {
   const course = await Course.findAll({
     where: {
@@ -50,7 +73,8 @@ router.get('/courses/:id', async (req, res) => {
   res.json(course);
 });
 
-router.post('/courses', async (req, res, next) => {
+//creates a course, sets the Location header to the URI for the course, and returns no content
+router.post('/courses', authenticatedUser, async (req, res, next) => {
   try {
     const course = req.body;
     const newCourse = await Course.create(course)
@@ -62,31 +86,27 @@ router.post('/courses', async (req, res, next) => {
   };
 });
 
+//updates a course and returns no content
 router.put('/courses/:id', async (req, res) => {
-  try{
     const course = await Course.findByPk(req.params.id)
-    if(course) {
-      await course.update(req.body)
+    if(course.userId === req.body.userId) {
+      await course.update(req.body);
+      res.status(204).end();
     } else {
-      res.status(404).json({message: 'oh no'})
+     res.status(401).json({message: "You're not authorized to delete this course."});
     }
-    res.status(204).end()
-  } catch (error) {
-    if (error.name === 'SequelizeValiationError') {
-      res.status(404).json({error: error.message})
-    } else { 
-      return next(error)
-    }
-  } 
-
 })
 
-// router.delete()
+//deletes a course and returns no content
+router.delete('/courses/:id', authenticatedUser, async (req, res, next) => { 
+    const course = await Course.findByPk(req.params.id)
+    if(course.userId === req.body.userId) {
+     await course.destroy();
+     res.status(204).end();
+    }
+    else {
+      res.status(401).json({message: "You're not authorized to delete this course."});
+    }
+})
 
-//if (course) {
-    // res.json(course);
-    // } else {
-    //   res.json({ error: 'Course not found' })
-    // }
-    
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,26 +13,58 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
+/*
+ * authenticating the user
+*/
+
+const authenticatedUser = async (req, res, next) => {
+  let message = null;
+ const credentials = auth(req);
+ if (credentials) {
+   const user = await User.findOne({ where: {emailAddress: credentials.name} });
+   if (user) {
+     const authenticated = bcryptjs
+       .compareSync(credentials.pass, user.password);
+     if (authenticated) {
+       req.currentUser = user;
+     } else {
+       message = `Authentication failure for username: ${user.username}`;
+     }
+   } else {
+     message = `User not found for username: ${credentials.name}`;
+   }
+ } else {
+   message = 'Auth header not found';
+ }
+ if (message) {
+   console.warn(message);
+   res.status(401).json({ message: 'Access Denied' });
+ } else {
+   next();
+ }
+};
+
+/*
+ * ROUTES *
+*/
+
 //returns the currently authenticated user
-router.get('/users', async (req, res) => {
-  const userData = await User.findAll()
+router.get('/users', authenticatedUser, (req, res) => {
+  const userData = req.currentUser;
   res.json(userData);
 });
 
 //creates a user, sets the location header to '/', & returns no content
-router.post('/users', async (req, res, next) => {
+router.post('/users',  async (req, res, next) => {
  try {
-    const { firstName, lastName, emailAddress, password} = req.body;
-
-    const result = await User.create({
-      firstName,
-      lastName,
-      emailAddress,
-      password
-    })
-    res.location('/').status(201);
-    res.json({id: result.id})
-
+    if(req.body.password){
+      req.body.password = await bcryptjs.hashSync(req.body.password);
+      await User.create(req.body);
+    } else {
+      await User.create(req.body);
+    }
+    res.location('/');
+    res.status(201).end();
   } catch (err) {
       err.message = err.errors.map(val => val.message)   
       res.status(400)
@@ -41,5 +73,3 @@ router.post('/users', async (req, res, next) => {
 });
 
 module.exports = router;
-
-/** */
